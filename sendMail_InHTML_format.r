e903248a11e45e5f06ece746f0c85f0c1bd5e459
Alex <jingwang512@gmail.com>
2017-12-01 09:16:00 +0800

    Update sendMail_InHTML_format.r

--- sendMail_InHTML_format.r
+++ sendMail_InHTML_format.r
@@ -1,18 +1,33 @@
+# set working directory
+setwd("F:\\Console_WOF\\RApps\\weekly_report")
+
+library(rmarkdown)
+library(htmlTable)
+library(readtext)
+library(stringr)
+
+# pandoc version 1.12.3 or higher is required and was not found
+# render rmd file
+Sys.setenv(RSTUDIO_PANDOC = "D:\\Program Files\\RStudio\\bin\\pandoc")
+rmarkdown::render("wof_weekly_report.Rmd")
+
 library(RDCOMClient)
 # create outlook app
 today <- Sys.Date()
-title <- paste0('[WOF][JEO] Marketing Report Week of ',dt_start, ' ~ ', dt_end)
+title <- paste0('[WOF][JEO] Marketing Report Week of ', dt_start, ' ~ ', dt_end)
 
 OutApp <- COMCreate("Outlook.Application")
 outMail = OutApp$CreateItem(0)
 
 # yu.wang3@ubisoft.com
-outMail[["To"]] = "jing.wang@ubisoft.com"
-outMail[["CC"]] = "jing.wang@ubisoft.com"
+outMail[["To"]] = "xxx@xxxx.com"
+outMail[["CC"]] = "xxx@xxx.com"
 outMail[["subject"]] = title
 
 # attach all of png files
 charts <- list.files() %>% str_subset(".png")
+charts <- paste0(getwd(),'/',charts) # have to attach the full path when attch file as attachment
+
 for (a in charts) {
     cht = a
     outMail[["Attachments"]]$Add(cht)
@@ -21,13 +36,10 @@ for (a in charts) {
 # build HTML body
 library(readtext)
 htmlbody <- readtext("wof_weekly_report_format.txt")
-
 MyHTML <- htmlbody$text
 
-value_list_wof <- c("wof_total", "wof_total_x1", "wof_total_ps4", "wof_sessions_x1", "wof_sessions_ps4")
-value_list_jeo <- c("jeo_total", "jeo_total_x1", "jeo_total_ps4", "jeo_sessions_x1", "jeo_sessions_ps4")
-value_list <- c(value_list_wof, value_list_jeo)
-
+# variable list is stored in file wof_weekly_report_variable_list.txt
+value_list <- read.csv("wof_weekly_report_variable_list.txt", header = F, col.names = "variable", stringsAsFactors = F)$variable
 # update values in the html script
 for (l in value_list) {
     patt <- l
